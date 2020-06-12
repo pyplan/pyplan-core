e4e084dcef1c4cb4954232ddcec9b60ba99f586b
Fabian <fbrussa@novix.com>
2020-06-12 15:52:02 -0300

    Rname inndex item Wizard

---
+++ pyplan_core/classes/wizards/RenameIndexItem.py
@@ -0,0 +1,47 @@
+from pyplan_core.classes.wizards.BaseWizard import BaseWizard
+from pyplan_core.classes.evaluators.XArrayEvaluator import XArrayEvaluator
+from pandas import Index
+import json
+
+
+class Wizard(BaseWizard):
+
+    def __init__(self):
+        self.code = "RenameIndexItem"
+
+    def getIndexItems(self, model, params):
+        nodeId = params["nodeId"]
+        res = []
+        if model.existNode(nodeId):
+            base_node = model.getNode(nodeId)
+            pd_index = base_node.result
+            if isinstance(pd_index, Index):
+                res = pd_index.values.tolist()
+        return res
+
+    def generateDefinition(self, model, params):
+        nodeId = params["nodeId"]
+        if model.existNode(nodeId):
+            base_node = model.getNode(nodeId)
+            index_values_str = params["values"]
+            index_values = json.loads(index_values_str)
+            for output_node in base_node.outputs:
+                if model.existNode(output_node) and model.getNode(output_node).nodeClass == "inputtable":
+                    input_table_node = model.getNode(output_node)
+                    da = input_table_node.result  # to force apply previous changes
+                    if nodeId in da.coords:
+                        da.coords[nodeId] = index_values
+
+                    # generate new definition
+                    evaluator = XArrayEvaluator()
+                    input_table_node._result = da
+                    new_def = evaluator.generateNodeDefinition(
+                        model.nodeDic, output_node)
+
+                    # test and set new definition:
+                    _ = model.evaluate(new_def)
+                    input_table_node.definition = new_def
+            base_node.definition = f"result = pd.Index({index_values_str})"
+            return nodeId
+
+        return ""
