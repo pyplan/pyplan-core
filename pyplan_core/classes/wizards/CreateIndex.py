49be2e84c020e13a032b8d1c267a8b5726bc2563
Fabian <fbrussa@novix.com>
2020-06-23 18:37:41 -0300

    fix encoding

--- pyplan_core/classes/wizards/CreateIndex.py
+++ pyplan_core/classes/wizards/CreateIndex.py
@@ -15,19 +15,19 @@ class Wizard(BaseWizard):
 
             if params["type"] == "list":
                 index_values = params["values"]
-                index_values_str = json.dumps(index_values)
+                index_values_str = json.dumps(index_values, ensure_ascii=False)
                 new_def = f"result = pd.Index({index_values_str})"
             elif params["type"] == "range":
                 if params["rangeType"] == "string":
                     prefix = params["stringPrefix"]
                     index_values = [f'{prefix}{nn}' for nn in range(
                         int(params["from"]), int(params["to"])+1, int(params["step"]))]
-                    str_values = json.dumps(index_values)
+                    str_values = json.dumps(index_values, ensure_ascii=False)
                     new_def = f"result = pd.Index({str_values})"
                 elif params["rangeType"] == "numeric":
                     index_values = list(range(int(params["from"]), int(
                         params["to"])+1, int(params["step"])))
-                    str_values = json.dumps(index_values)
+                    str_values = json.dumps(index_values, ensure_ascii=False)
                     new_def = f"result = pd.Index({str_values})"
                 elif params["rangeType"] == "date":
                     freq = params["freq"]
